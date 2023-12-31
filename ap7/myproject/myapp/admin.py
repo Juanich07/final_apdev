af03ea6a64cb792c79ce1a254488ac09203a46d6
Juanich07 <202180228@psu.palawan.edu.ph>
2023-12-31 09:51:54 +0800

    iwankona

--- ap7/myproject/myapp/admin.py
+++ ap7/myproject/myapp/admin.py
@@ -11,6 +11,44 @@
 # admin.site.register(Continent)
 # myapp/admin.py
 from django.contrib import admin
+# from .models import City, Country, CountryLanguage, Continent, Region, GovernmentForm
+
+# @admin.register(City)
+# class CityAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'country_code', 'district', 'population']
+
+# @admin.register(Country)
+# class CountryAdmin(admin.ModelAdmin):
+#     list_display = ['code', 'name', 'continent', 'region', 'surface_area', 'indep_year',
+#                     'population', 'life_expectancy', 'gnp', 'gnp_old', 'local_name', 
+#                     'government_form', 'head_of_state', 'capital', 'code2']
+
+# @admin.register(CountryLanguage)
+# class CountryLanguageAdmin(admin.ModelAdmin):
+#     list_display = ['country_code', 'language', 'is_official', 'percentage']
+
+# @admin.register(Continent)
+# class ContinentAdmin(admin.ModelAdmin):
+#     list_display = ['name']
+
+# @admin.register(Region)
+# class RegionAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'continent']
+
+# @admin.register(GovernmentForm)
+# class GovernmentFormAdmin(admin.ModelAdmin):
+#     list_display = ['name']
+# @admin.register(Country)
+# class CountryAdmin(admin.ModelAdmin):
+#     list_display = ['code', 'name', 'get_continents', 'region', 'surface_area', 'indep_year',
+#                     'population', 'life_expectancy', 'gnp', 'gnp_old', 'local_name', 
+#                     'government_form', 'head_of_state', 'capital', 'code2']
+
+#     def get_continents(self, obj):
+#         return ', '.join([continent.name for continent in obj.continents.all()])
+
+#     get_continents.short_description = 'Continents'
+from django.contrib import admin
 from .models import City, Country, CountryLanguage, Continent, Region, GovernmentForm
 
 @admin.register(City)
@@ -19,9 +57,14 @@ class CityAdmin(admin.ModelAdmin):
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ['code', 'name', 'continent', 'region', 'surface_area', 'indep_year',
+    list_display = ['code', 'name', 'get_continents', 'region', 'surface_area', 'indep_year',
                     'population', 'life_expectancy', 'gnp', 'gnp_old', 'local_name', 
                     'government_form', 'head_of_state', 'capital', 'code2']
+
+    def get_continents(self, obj):
+        return ', '.join([continent.name for continent in obj.continents.all()])
+
+    get_continents.short_description = 'Continents'
 
 @admin.register(CountryLanguage)
 class CountryLanguageAdmin(admin.ModelAdmin):
--- ap7/myproject/myapp/models.py
+++ ap7/myproject/myapp/models.py
@@ -51,6 +51,77 @@
 #     def __str__(self):
 #         return f"{self.country_code} - {self.language}"
 # myapp/models.py
+# from django.db import models
+# from django.utils import timezone
+
+# class BaseModel(models.Model):
+#     created_at = models.DateTimeField(default=timezone.now)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     class Meta:
+#         abstract = True
+#         app_label = 'myapp'
+
+# class City(BaseModel):
+#     name = models.CharField(max_length=100)
+#     country_code = models.CharField(max_length=3)
+#     district = models.CharField(max_length=100)
+#     population = models.IntegerField()
+
+#     def __str__(self):
+#         return self.name
+
+# class Country(BaseModel):
+#     code = models.CharField(max_length=3, unique=True)
+#     name = models.CharField(max_length=100)
+#     continent = models.CharField(max_length=50)
+
+#     surface_area = models.FloatField()
+#     indep_year = models.IntegerField(null=True, blank=True)
+#     population = models.IntegerField()
+#     life_expectancy = models.FloatField(null=True, blank=True)
+#     gnp = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+#     gnp_old = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+#     local_name = models.CharField(max_length=100)
+#     government_form = models.CharField(max_length=100)
+#     head_of_state = models.CharField(max_length=100)
+#     capital = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True)
+#     code2 = models.CharField(max_length=2)
+#     flag = models.ImageField(upload_to='country_flags/', null=True, blank=True)
+
+#     def __str__(self):
+#         return self.name
+
+# class CountryLanguage(BaseModel):
+#     country_code = models.ForeignKey(Country, on_delete=models.CASCADE)
+#     language = models.CharField(max_length=50)
+#     is_official = models.BooleanField()
+#     percentage = models.DecimalField(max_digits=5, decimal_places=2)
+
+#     def __str__(self):
+#         return f"{self.country_code} - {self.language}"
+
+# class Continent(BaseModel):
+#     name = models.CharField(max_length=50, unique=True)
+
+#     def __str__(self):
+#         return self.name
+
+# class Region(BaseModel):
+#     name = models.CharField(max_length=50, unique=True)
+#     continent = models.ForeignKey(Continent, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return f"{self.name} ({self.continent})"
+
+# class GovernmentForm(BaseModel):
+#     name = models.CharField(max_length=100, unique=True)
+
+#     def __str__(self):
+#         return self.name
+
+# models.py
+
 from django.db import models
 from django.utils import timezone
 
@@ -71,10 +142,16 @@ class City(BaseModel):
     def __str__(self):
         return self.name
 
+class Continent(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Country(BaseModel):
     code = models.CharField(max_length=3, unique=True)
     name = models.CharField(max_length=100)
-    continent = models.CharField(max_length=50)
+    continents = models.ManyToManyField(Continent)  # Many-to-Many relationship
     region = models.CharField(max_length=50)
     surface_area = models.FloatField()
     indep_year = models.IntegerField(null=True, blank=True)
@@ -88,7 +165,8 @@ class Country(BaseModel):
     capital = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True)
     code2 = models.CharField(max_length=2)
     flag = models.ImageField(upload_to='country_flags/', null=True, blank=True)
-
+    continents = models.ManyToManyField(Continent, blank=True)
+    # continents = models.ManyToManyField(Continent, default= <your_default_continent>)
     def __str__(self):
         return self.name
 
@@ -100,12 +178,6 @@ class CountryLanguage(BaseModel):
 
     def __str__(self):
         return f"{self.country_code} - {self.language}"
-
-class Continent(BaseModel):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
 
 class Region(BaseModel):
     name = models.CharField(max_length=50, unique=True)
@@ -119,4 +191,3 @@ class GovernmentForm(BaseModel):
 
     def __str__(self):
         return self.name
-
